a20d7f44234c31c38e60c49567b93529cdf2f830
Matias Alonso Godoy Azagra <matias.godoy.a@ug.uchile.cl>
2022-09-07 17:52:29 -0300

    agrega mis soluciones de la tarea 3 y otras anteriores

--- 03_inverse_kinematics/inverse_kinematics.py
+++ 03_inverse_kinematics/inverse_kinematics.py
@@ -1,10 +1,26 @@
+from math import *
+
+
 def position_to_dof(x, y, z):
-    """Acá va tu código para calcular la cinemática inversa
-    
-    La función debe recibir (x,y,z) en milimetros y retornar q0, q1 y q2 en grados"""
+    """La función debe recibir (x,y,z) en milimetros y retornar q0, q1 y q2 en grados
+    El robot solo entiende grados sexagesimales y enteros"""
+    a0, a1 = 94, 125
+    a2, a3 = 147, 60.3
 
-    q0 = 0  # reemplaza el cero por la ecuación que describe q0 en función de (x,y)
-    q1 = 0  # reemplaza el cero por la ecuación que describe q0 en función de (x,y,z)
-    q2 = 0  # reemplaza el cero por la ecuación que describe q0 en función de (x,y,z)
+    q0 = atan(y/x)
+    q2 = acos(((x/cos(q0)-a3)**2+(z-a0)**2-a1**2-a2**2)/(2*a1*a2))
+    q1 = atan((z-a0)/(x/cos(q0)-a3))-atan((a2*sin(q2))/(a1+a2*cos(q2)))
 
-    return int(q0), int(q1), int(q2)    # El robot solo entiende grados sexagesimales y enteros 
+    return int(q0*180/pi), int(q1*180/pi), int(q2*180/pi)
+
+
+def conversion(q0, q1, q2):
+    a0, a1 = 94, 125
+    a2, a3 = 147, 60.3
+    Q0, Q1, Q2 = radians(q0), radians(q1), radians(q2)
+
+    x = (a1*cos(Q1)+a2*cos(Q1+Q2)+a3)*cos(Q0)
+    y = (a1*cos(Q1)+a2*cos(Q1+Q2)+a3)*sin(Q0)
+    z = a0+a1*(sin(Q1))+a2*sin(Q1+Q2)
+
+    return x, y, z
--- 03_inverse_kinematics/move_the_robot.py
+++ 03_inverse_kinematics/move_the_robot.py
@@ -3,21 +3,25 @@ from client import RobotClient
 from inverse_kinematics import position_to_dof
 
 
-## Conectarse al robot
+# Conectarse al robot
 
 r = RobotClient(address="localhost")  # Recuerda usar una dirección válida
 r.connect()
-r.home()    # Revisa el archivo client.py para que veas qué hace esta función
+r.home()
 
 
-## Función para mover el robot usando cartesianas
 def move_robot_to_xyz(robot, x, y, z):
+    """Función para mover el robot usando cartesianas"""
     q0, q1, q2 = position_to_dof(x, y, z)
     robot.set_joints(q0, q1, q2)
 
 
-## Mover el robot (acá va tu código)
-move_robot_to_xyz(r, x=200, y=0, z=230)
-
-
-
+# Mover el robot (Origen x=185, y=0, z=241; q0 = 0, q1 = 0, q2 = 90)
+move_robot_to_xyz(r, x=205, y=-20, z=241)
+time.sleep(0.5)
+move_robot_to_xyz(r, x=205, y=20, z=241)
+time.sleep(0.5)
+move_robot_to_xyz(r, x=225, y=20, z=241)
+time.sleep(0.5)
+move_robot_to_xyz(r, x=225, y=-20, z=241)
+time.sleep(0.5)
